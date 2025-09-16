756a4124ddd1894cab2b34332ef555b2d0bfc404
Tairesu <tyresecook25@yahoo.com>
2025-09-16 10:02:42 -0500

    Added nav dropdown JS

--- assets/js/nav-dropdown.js
+++ assets/js/nav-dropdown.js
@@ -1,13 +1,29 @@
+
 $('#dropdown-btn').on('click', function () {
-  alert("hi");
     const panel = $('#nav-dropdown');
+    const svg = $('#dropdown-btn svg');
+    const border = $('#nav-border');
     
     if (panel.is(':visible')) {
       panel.slideUp(300);
+
+      svg
+        .removeClass('fill-primary')
+        .addClass('fill-black');
+      border
+        .removeClass('border-primary')
+        .addClass('border-greyed');
     } else {
       panel
         .css('display', 'flex') // <-- keep flex
         .hide()
         .slideDown(300);
+      svg
+        .removeClass('fill-black')
+        .addClass('fill-primary');
+      border
+        .removeClass('border-greyed')
+        .addClass('border-primary');
     }
+    console.log(svg);
   });
